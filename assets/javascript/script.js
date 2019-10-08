b34034e4713d97ff213337333e7d6e0782830c99
Blake Sykes <blakesykes13@gmail.com>
2019-10-07 23:02:24 -0600

    added function to create an array from the master array, to later be used with charts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,81 +1,107 @@
 // script.js
 var test = "JS READY"
 console.log("script.js " + test)
-// 1.0 - Initialize FIREBASE
-// 1.1 - Get from FIREBASE
+// 2.0 - Initialize FIREBASE
+// 2.1 - Get from FIREBASE
+// 2.2 - Import CSV
 
-// 2.0 - Import CSV
+var masterArray = [];
 
 
-
-
-// 1.0 |---- Bringing in the FIREBASE -------------------------------------------------------------------------------------------------|
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyByzGwJGJkA44iaOR9HBbNmTiNFo4trwd4",
-    authDomain: "p1fbase.firebaseapp.com",
-    databaseURL: "https://p1fbase.firebaseio.com",
-    projectId: "p1fbase",
-    storageBucket: "p1fbase.appspot.com",
-    messagingSenderId: "645233672545",
-    appId: "1:645233672545:web:d237ab2b51ffb7ac574520"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-  var database = firebase.database()
-
-  $(document).ready(function() {
-// 1.1 |---- Get Data from FIREBASE -------------------------------------------------------------------------------------------------|
-    
-    database.ref().on("value", function(snapshot) {
-        var masterArray = snapshot.val().master;
-        console.log(masterArray);
-    
-    })
-
-// 2.0 |---- Import CSV --------------------------------------------------------------------------------------------------------------|
-
-    // The event listener for the file upload
-    document.getElementById('txtFileUpload').addEventListener('change', upload, false);
-
-    // Method that checks that the browser supports the HTML5 File API
-    function browserSupportFileUpload() {
-        var isCompatible = false;
-        if (window.File && window.FileReader && window.FileList && window.Blob) {
-        isCompatible = true;
-        }
-        return isCompatible;
+// 1.0 |---- Create Array Function ---------------------------------------------------------------------------------------------------|
+// Loop through master array to get column # from each array within. Repeat for b.
+function createArray( a , b ) {
+    var newArray = [];
+    var x = [];
+    var y = [];
+    for(i = 1; i < masterArray.length; i++){
+        x.push(masterArray[i][a]);
+        y.push(masterArray[i][b]);   
     }
+    newArray.push(x);
+    newArray.push(y);
+    return(newArray);
+}
 
-    // Method that reads and processes the selected file
-    function upload(evt) {
-        if (!browserSupportFileUpload()) {
-            alert('The File APIs are not fully supported in this browser!');
-            } else {
-                var data = null;
-                var file = evt.target.files[0];
-                var reader = new FileReader();
-                reader.readAsText(file);
-                reader.onload = function(event) {
-                    var csvData = event.target.result;
-                    data = $.csv.toArrays(csvData);
-                    if (data && data.length > 0) {
-                    alert('Imported -' + data.length + '- rows successfully!');
-                // If upload successful, push data arrays to firebase
-                        console.log(data)
-                            database.ref().set({
-                                master: data
-                            });
-                    } else {
-                        alert('No data to import!');
-                    }
-                };
-                reader.onerror = function() {
-                    alert('Unable to read ' + file.fileName);
-                };
-                console.log("FB " + database)
+
+
+
+// 2.0 |---- On Ready ---------------------------------------------------------------------------------------------------------------|
+
+$(document).ready(function() {
+
+    // 2.1 |---- Bringing in the FIREBASE ------------------------------------------------------------------------------------------|
+    
+      // Your web app's Firebase configuration
+      const firebaseConfig = {
+        apiKey: "AIzaSyByzGwJGJkA44iaOR9HBbNmTiNFo4trwd4",
+        authDomain: "p1fbase.firebaseapp.com",
+        databaseURL: "https://p1fbase.firebaseio.com",
+        projectId: "p1fbase",
+        storageBucket: "p1fbase.appspot.com",
+        messagingSenderId: "645233672545",
+        appId: "1:645233672545:web:d237ab2b51ffb7ac574520"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+    
+      var database = firebase.database()
+    
+    // 2.2 |---- Get Data from FIREBASE -------------------------------------------------------------------------------------------------|
+        
+        database.ref().on("value", function(snapshot) {
+            masterArray = snapshot.val().master;
+            console.log(masterArray);
+            // Create new array from Master
+            var array1 = createArray(0,1);
+            console.log(array1);
+        
+        })
+    
+    // 2.3 |---- Import CSV --------------------------------------------------------------------------------------------------------------|
+    
+        // The event listener for the file upload
+        document.getElementById('txtFileUpload').addEventListener('change', upload, false);
+    
+        // Method that checks that the browser supports the HTML5 File API
+        function browserSupportFileUpload() {
+            var isCompatible = false;
+            if (window.File && window.FileReader && window.FileList && window.Blob) {
+            isCompatible = true;
             }
+            return isCompatible;
         }
-    });
+    
+        // Method that reads and processes the selected file
+        function upload(evt) {
+            if (!browserSupportFileUpload()) {
+                alert('The File APIs are not fully supported in this browser!');
+                } else {
+                    var data = null;
+                    var file = evt.target.files[0];
+                    var reader = new FileReader();
+                    reader.readAsText(file);
+                    reader.onload = function(event) {
+                        var csvData = event.target.result;
+                        data = $.csv.toArrays(csvData);
+                        if (data && data.length > 0) {
+                        alert('Imported -' + data.length + '- rows successfully!');
+                    // If upload successful, push data arrays to firebase
+                            console.log(data)
+                                database.ref().set({
+                                    master: data
+                                });
+                        } else {
+                            alert('No data to import!');
+                        }
+                    };
+                    reader.onerror = function() {
+                        alert('Unable to read ' + file.fileName);
+                    };
+                    console.log("FB " + database)
+                }
+            }
+
+
+        });
+    
