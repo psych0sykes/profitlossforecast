0f815b61fbb24ec5c65be0d8b6f256fda2841e01
Blake Sykes <blakesykes13@gmail.com>
2019-10-07 21:11:11 -0600

    added csv import, with script. Added Firebase to script.

---
+++ assets/javascript/script.js
@@ -0,0 +1,21 @@
+// script.js
+// 1 - FIREBASE
+
+
+
+
+
+// |---- Bringing in the FIREBASE -------------------------------------------------------------------------------------------------|
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyByzGwJGJkA44iaOR9HBbNmTiNFo4trwd4",
+    authDomain: "p1fbase.firebaseapp.com",
+    databaseURL: "https://p1fbase.firebaseio.com",
+    projectId: "p1fbase",
+    storageBucket: "",
+    messagingSenderId: "645233672545",
+    appId: "1:645233672545:web:d237ab2b51ffb7ac574520"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
